eee5ef87e9c7bbaf397963140b229442a001d68c
molchiro <40120093+molchiro@users.noreply.github.com>
2018-06-18 22:48:49 +0900

    Create FizzBuzz.js

---
+++ 1/FizzBuzz.js
@@ -0,0 +1,18 @@
+/*
+ググったこと
+・標準入力のやり方
+*/
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var input_string = '';
+ 
+process.stdin.on('data', function(chunk) {
+  input_string += chunk;
+});
+ 
+process.stdin.on('end', function() {
+    // ここで処理を処理を記述
+    console.log(input_string);
+});
