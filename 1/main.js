6c86b61746b3fe9379318b352872d10059acd449
molchiro <40120093+molchiro@users.noreply.github.com>
2018-06-19 18:59:40 +0900

    Update main.js

--- 1/main.js
+++ 1/main.js
@@ -2,7 +2,7 @@ var myButton = document.querySelector('button');
 
 function FizzBuzz() {
  var inputNum = prompt("数字を入力して下さい");
- if (inputNum = Null) {
+ if (inputNum === "") {
   return;
  };
  var outputStr = "";
