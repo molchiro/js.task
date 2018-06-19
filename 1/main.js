7eabf93a00106c35d768b2af45f45c4e819cb7a9
molchiro <40120093+molchiro@users.noreply.github.com>
2018-06-19 18:28:20 +0900

    Update main.js

--- 1/main.js
+++ 1/main.js
@@ -1,13 +1,14 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+var myButton = document.querySelector('button');
 
-var input_string = '';
- 
-process.stdin.on('data', function(chunk) {
-  input_string += chunk;
-});
- 
-process.stdin.on('end', function() {
-    // ここで処理を処理を記述
-    console.log(input_string);
-});
+function FizzBuzz() {
+ var inputNum = prompt("数字を入力して下さい");
+ if (inputNum = Null) {
+  return;
+ };
+ var outputStr = "";
+ alert(inputNum);
+}
+
+myButton.onclick = function() {
+  FizzBuzz();
+}
