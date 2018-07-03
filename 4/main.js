78163301ab211e4229cceee3b4079576c9b7eaa4
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-07-03 22:36:31 +0900

    課題4初稿

---
+++ 4/main.js
@@ -0,0 +1,21 @@
+const myButton = document.querySelector('button');
+
+const sumUpToOne = function(x) {
+  // x + (x-1) + (x-2) + ... + 2 + 1
+  if (x < 1) return 0;
+  return x + sumUpToOne(x-1);
+}
+
+myButton.onclick = function() {
+  // ユーザ入力
+  const input = prompt("正の整数を入力して下さい");
+  if (input === null) return;
+  const inputNum = Number(input)
+  // 入力チェック
+  if (inputNum < 1 || inputNum !== parseInt(inputNum)) {
+    alert("入力が正の整数ではありません");
+    return;
+  }
+  // 結果表示
+  alert(sumUpToOne(inputNum));
+}
