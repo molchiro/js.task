28b97fdf0755dd3039fedfd4e2a2bf39de390f8c
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-07-14 14:41:19 +0900

    機能の拡大 大文字小文字の区別をなくし、句読点等があっても動作するようにした

--- 2/main.js
+++ 2/main.js
@@ -1,7 +1,7 @@
 const myButton = document.querySelector('button');
 
-myButton.onclick = function() {
-  const inputStr = prompt("短文を入力して下さい");
+myButton.onclick = () => {
+  const inputStr = prompt("英文を入力して下さい");
 
   if (inputStr === null) {
     return;
@@ -9,12 +9,12 @@ myButton.onclick = function() {
     return;
   }
 
+  const trimedStr = inputStr.toLowerCase().replace(/[^0-9a-z ]/g, '');
+
   // 新出単語の場合はハッシュ配列に初期値１で追加
   // 既出は+1
-  // ToDo:'.'や'?'の扱い
-  // ToDo:大文字小文字の区別をなくす
   let hashWords = {};
-  for (word of inputStr.split(" ")) {
+  for (let word of trimedStr.split(" ")) {
     if (word in hashWords) {
       hashWords[word] += 1;
     } else {
