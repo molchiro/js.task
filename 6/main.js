11feef1cbfa9f15ed4621ef7269d02fb10fe8e9b
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-07-24 22:03:50 +0900

    ユーザ入力の処理変更 myPromptの再帰的呼び出しをやめ、無限ループによる素直な実装に変更

--- 6/main.js
+++ 6/main.js
@@ -1,32 +1,36 @@
 const myButton = document.querySelector('button');
 
-const myPrompt = (msg) => {
-  // 正しい入力値でなければ再度prompt
-  const ERROR_MSG = '入力エラー\n0-100の数字を入力して下さい\n(キャンセルで終了します)'
-  const input = prompt(msg);
-  if (input === null) {
-    return 'cancel'
-  } else if (input === '') {
-    return myPrompt(ERROR_MSG);
-  } else if (! (input > -1 && input < 101)) {
-    return myPrompt(ERROR_MSG);
-  }
-  return parseInt(input);
-}
-
 myButton.onclick = () => {
-  const ans = Math.floor(Math.random() * 101 );
   let input = '';
   let cnt = 0;
-  let msg = '0-100の数字を入力して下さい';
+  let msg = '0-100の整数を入力して下さい';
+  const ERROR_MSG = '入力エラー\n0-100の整数を入力して下さい\n(キャンセルで終了します)'
+
+  const ans = Math.floor(Math.random() * 101 ); //0-100
 
   while (true) {
     cnt++;
-    input = myPrompt(msg + '\n(キャンセルで終了します)');
-    if (input === 'cancel') {return;}
-    if (input > ans) {
+    // ユーザ入力
+    while(true) {
+      input = prompt(msg + '\n(キャンセルで終了します)');
+      if (input === null) {return;}
+      // 入力値チェック
+      input = Number(input);
+      if (input === '') {
+        alert(ERROR_MSG);
+      } else if (! (input > -1 && input < 101)) {
+        alert(ERROR_MSG);
+      } else if (input !== parseInt(input)) {
+        alert(ERROR_MSG);
+      } else {
+        break;
+      }
+    }
+    // 結果表示
+    // 外れ時のメッセージは、ここでは生成のみ。次のループで表示。
+    if (parseInt(input) > ans) {
       msg = 'もっと下'
-    } else if (input < ans)  {
+    } else if (parseInt(input) < ans)  {
       msg = 'もっと上'
     } else {
       alert('正解！\n'+cnt+'回で当てました。')
--- 7/main.js
+++ 7/main.js
@@ -1,23 +1,12 @@
 const myButton = document.querySelector('button');
 
-const myPrompt = (msg) => {
-  // 正しい入力値でなければ再度prompt
-  const ERROR_MSG = '入力エラー\n1000-9999の数字を入力して下さい\n(キャンセルで終了します)'
-  const input = prompt(msg);
-  if (input === null) {
-    return 'cancel'
-  } else if (input === '') {
-    return myPrompt(ERROR_MSG);
-  } else if (! (input > 999 && input < 10000)) {
-    return myPrompt(ERROR_MSG);
-  }
-  return parseInt(input);
-}
-
 const fourDigitNum = () => {
   // 要素に重複のない４桁の数字を返す
-  // 条件を満たすまで再起し、ランダムな1000-9999の数字を生成
-  const num = Math.floor(Math.random() * 9000 ) + 1000; //1000-9999
+
+  //1000-9999のランダムな値を生成
+  const num = Math.floor(Math.random() * 9000 ) + 1000;
+
+  // 条件チェック　要件を満たさなければ再起でリトライ
   const numStr= String(num);
   for (let i = 0; i < 3; i++) {
     // ex:1234, first "234" vs "1", next "34" vs "2", last "3" vs "4"
@@ -29,9 +18,13 @@ const fourDigitNum = () => {
 }
 
 const countHitBlow = (input, ans) => {
+  // ヒントの生成
+  // Hit:数と桁位置の両方が同じであること
+  // Blow:数だけが同じで桁位置が異なること
   const inputStr = String(input);
   const ansStr = String(ans);
   const hints = {'hit': 0, 'blow':0};
+
   for (let i = 0; i < 4; i++) {
     if (inputStr[i] === ansStr[i]) {
       hints['hit']++;
@@ -39,27 +32,45 @@ const countHitBlow = (input, ans) => {
       hints['blow']++;
     }
   }
+
   return hints;
 }
 
 myButton.onclick = () => {
-  const ans = fourDigitNum();
   let input = '';
   let cnt = 0;
   let msg = '1000-9999の数字を入力して下さい\n';
   let history = '';
+  let hints = {}
+  const ERROR_MSG = '入力エラー\n1000-9999の数字を入力して下さい\n(キャンセルで終了します)'
+
+  const ans = fourDigitNum();
 
   while (true) {
     cnt++;
-
-    input = myPrompt(msg + '(キャンセルで終了します)');
-    if (input === 'cancel') {return;}
-
+    // ユーザ入力
+    while(true) {
+      input = prompt(msg + '\n(キャンセルで終了します)');
+      if (input === null) {return;}
+      // 入力値チェック
+      input = Number(input);
+      if (input === '') {
+        alert(ERROR_MSG);
+      } else if (! (input > 999 && input < 10000)) {
+        alert(ERROR_MSG);
+      } else if (input !== parseInt(input)) {
+        alert(ERROR_MSG);
+      } else {
+        break;
+      }
+    }
+    // 結果表示
     if (input === ans) {
       alert('正解！\n'+cnt+'回で当てました。')
       return;
     } else {
-      let hints = countHitBlow(input, ans);
+      // 外れ時のメッセージは、ここでは生成のみ。次のループで表示。
+      hints = countHitBlow(input, ans);
       history = input+' '+hints['hit']+'Hit,'+hints['blow']+'blow\n'
                 + history;
       msg = '外れ！\n' + history;
