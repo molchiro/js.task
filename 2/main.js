b0243eb01289b3a4b1dc04dc78f4dc3af87cabd1
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-07-24 22:00:25 +0900

    コメントの拡充

--- 2/main.js
+++ 2/main.js
@@ -1,16 +1,17 @@
 const myButton = document.querySelector('button');
 
 myButton.onclick = () => {
+  // ユーザ入力
   const inputStr = prompt("英文を入力して下さい");
-
+  //  入力値チェック
   if (inputStr === null) {
     return;
   } else if (inputStr === "") {
     return;
   }
-
+  // 入力値のトリミング。全て小文字に直し、句読点を除く
   const trimedStr = inputStr.toLowerCase().replace(/[^0-9a-z ]/g, '');
-
+  // メイン処理
   // 新出単語の場合はハッシュ配列に初期値１で追加
   // 既出は+1
   let hashWords = {};
