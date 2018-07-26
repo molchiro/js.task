f84e66ca802125e12e40ae5c80a4a8e568a2e3be
maedana <maeda.na@gmail.com>
2018-07-26 19:19:32 +0900

    2018/07/26の指摘内容の反映

--- 1/main.js
+++ 1/main.js
@@ -1,28 +1,26 @@
 const myButton = document.querySelector('button');
 
-myButton.onclick = () => {
-  // ユーザ入力
+myButton.addEventListener('click', () => {
   const inputNum = prompt("数字を入力して下さい");
-  // 入力値チェック
+
   if (inputNum === null) {
     return;
   } else if (inputNum < 1) {
     alert("正の整数を入力して下さい");
     return;
   }
-  // メイン処理
-  let outputStr = "";
-  for (let i=1;i<inputNum;i++) {
+
+  let outputs = []
+  for (i=1;i<inputNum;i++) {
     if(i%15 === 0) {
-      outputStr += "FizzBuzz" + ', ';
+      outputs.push("FizzBuzz")
     } else if (i%3 === 0) {
-      outputStr += "Fizz" + ', ';
+      outputs.push("Fizz")
     } else if (i%5 === 0) {
-      outputStr += "Buzz" + ', ';
+      outputs.push("Buzz")
     } else {
-      outputStr += i + ', ';
+      outputs.push(i)
     }
   }
-  // HACK:もっと綺麗に', 'を除きたい
-  alert(outputStr.slice(0, outputStr.length - 2));
-}
+  alert(outputs.join(','))
+})
--- 3/main.js
+++ 3/main.js
@@ -1,34 +1,42 @@
 const myButton = document.querySelector('button');
+const SIGNS = {
+  0: "グー",
+  1: "チョキ",
+  2: "パー"
+}
 
-const janken = (msg) => {
-  // 戻り値　0:あいこ 1:勝ち 2:負け
-  // キャンセル時とエラー時は-1
-
-  const SIGNS = ["グー", "チョキ", "パー"];
-  const RES_MSG = ["あいこ!","あなたの勝ち!","あなたの負け！"];
-
+const janken = function(msg) {
   // ユーザ入力
-  while(true) {
-    const playerSign = prompt(msg + "\n(0:グー 1:チョキ 2:パー)");
-    // 入力チェック
-    if (playerSign === null) return -1;
-    if (!(playerSign in [0,1,2])) {
-      alert("0から2の数字で入力して下さい");
-    }
+  const playerSign = prompt(`${msg}\n(0:グー 1:チョキ 2:パー)`);
+  if (!playerSign) return -1;
+  // 入力チェック
+  if (!SIGNS[playerSign]) {
+    alert("0から2の数字で入力して下さい");
+    return -1;
   }
   // 勝敗計算
   // res=0:あいこ 1:勝ち 2:負け
   const comSign = Math.floor(Math.random() * 3 );
   const res = (comSign - playerSign + 3) % 3;
   // 勝敗表示
-  alert("com:" + SIGNS[comSign] + "\n"
-    + "you:" + SIGNS[playerSign] + "\n"
-    + RES_MSG[res]);
+  showResult(comSign, playerSign, res)
   return res;
 }
 
-myButton.onclick = () => {
-  // 勝敗が決まるまでjankenを無限に呼び出す。
+const showResult = (comSign, playerSign, res) => {
+  const resMsg = ["あいこ!","あなたの勝ち!","あなたの負け！"];
+
+  const messages = []
+  messages.push(`com:${SIGNS[comSign]}`)
+  messages.push(`you:${SIGNS[playerSign]}`)
+  messages.push(resMsg[res])
+  alert(messages.join("\n"))
+}
+
+myButton.onclick = function() {
+  // jankenの戻り値
+  // 0:あいこ 1:勝ち 2:負け
+  // キャンセル時とエラー時は-1
   let res = janken("じゃんけん...");
   while (res === 0) {
     res = janken("あいこで...")
