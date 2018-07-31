d6dad9a7cb75309d7628bfe08c84c9a94385f1e4
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-07-31 23:35:12 +0900

    リファクタリング 入力チェックを外に出す メッセージの生成と表示のコードがひとかたまりになるよう順序変更

--- 6/main.js
+++ 6/main.js
@@ -1,40 +1,42 @@
 const myButton = document.querySelector('button');
 
-myButton.onclick = () => {
+myButton.addEventListener('click', () => {
   let input = '';
   let cnt = 0;
-  let msg = '0-100の整数を入力して下さい';
-  const ERROR_MSG = '入力エラー\n0-100の整数を入力して下さい\n(キャンセルで終了します)'
+  let msg = '';
 
-  const ans = Math.floor(Math.random() * 101 ); //0-100
+  const ans = Math.floor(Math.random() * 101); //0-100
 
   while (true) {
     cnt++;
+    // メッセージ生成
+    if (cnt === 1) {
+      msg = '0-100の数字を入力して下さい';
+    } else {
+      msg = input > ans ? 'もっと下' : 'もっと上';
+    }
     // ユーザ入力
     while(true) {
       input = prompt(msg + '\n(キャンセルで終了します)');
       if (input === null) {return;}
-      // 入力値チェック
       input = Number(input);
-      if (input === '') {
-        alert(ERROR_MSG);
-      } else if (! (input > -1 && input < 101)) {
-        alert(ERROR_MSG);
-      } else if (input !== parseInt(input)) {
-        alert(ERROR_MSG);
-      } else {
+      if (checkInput(input)) {
         break;
+      } else {
+        alert('入力エラー\n0-100の整数を入力して下さい');
       }
     }
-    // 結果表示
-    // 外れ時のメッセージは、ここでは生成のみ。次のループで表示。
-    if (parseInt(input) > ans) {
-      msg = 'もっと下'
-    } else if (parseInt(input) < ans)  {
-      msg = 'もっと上'
-    } else {
+    // 判定
+    if (input === ans) {
       alert('正解！\n'+cnt+'回で当てました。')
       return;
     }
   }
+})
+
+const checkInput = (input) => {
+  if (!input) {return false;}
+  if (input < 0 || input > 100) {return false;}
+  if (input !== parseInt(input)) {return false;}
+  return true;
 }
